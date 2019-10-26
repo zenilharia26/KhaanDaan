6153875bbf6c950011ca6c455a5c74af25b25f41
Zenil Haria <zenil.haria@somaiya.edu>
2019-10-26 22:55:46 +0530

    Registration without models in Admin page.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from users.models import Restaurant, NGO
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -10,6 +12,14 @@ def register_restaurant(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('name')
+            restaurant = Restaurant()
+            restaurant.user = User.objects.get(email=form.cleaned_data.get('email'))
+            restaurant.name = form.cleaned_data.get('name')
+            restaurant.rid = form.cleaned_data.get('user_id')
+            restaurant.mobile_no = form.cleaned_data.get('mobile')
+            restaurant.email_id = form.cleaned_data.get('email')
+            restaurant.address = form.cleaned_data.get('address')
+            restaurant.save()
             messages.success(request,f'Account created successfully for {username}!')
             return redirect('login')
 
@@ -25,6 +35,14 @@ def register_ngo(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('name')
+            ngo.user = User.objects.get(User.objects.get(email=form.cleaned_data.get('email')))
+            ngo = NGO()
+            ngo.name = form.cleaned_data.get('name')
+            ngo.nid = form.cleaned_data.get('user_id')
+            ngo.mobile_no = form.cleaned_data.get('mobile')
+            ngo.email_id = form.cleaned_data.get('email')
+            ngo.address = form.cleaned_data.get('address')
+            ngo.save()
             messages.success(request,f'Account created successfully for {username}!')
             return redirect('login')
 
