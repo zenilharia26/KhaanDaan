c6a884591213be3a2ac1ee7eda7e83340a521c2d
arghyadeep99 <arghyadeep.d@somaiya.edu>
2019-10-28 11:29:11 +0530

    Dashboard changes for NGOs

--- users/views.py
+++ users/views.py
@@ -25,52 +25,5 @@ def register(request):
 
     else:
         form = UserRegisterForm()
-
         
     return render(request, 'users/register.html', {'form':form, 'name':'Restaurant'})
-
-def register_restaurant(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username=form.cleaned_data.get('name')
-            restaurant = KhaanDaanUsers()
-            restaurant.user = User.objects.get(email=form.cleaned_data.get('email'))
-            restaurant.name = form.cleaned_data.get('name')
-            #restaurant.uid = form.cleaned_data.get('uid')
-            restaurant.mobile_no = form.cleaned_data.get('mobile')
-            restaurant.email_id = form.cleaned_data.get('email')
-            restaurant.address = form.cleaned_data.get('address')
-            restaurant.user_type = form.cleaned_data.get('user_type')
-            restaurant.save()
-            messages.success(request,f'Account created successfully for {username}!')
-            return redirect('login')
-
-    else:
-        form = UserRegisterForm()
-
-        
-    return render(request, 'users/register.html', {'form':form, 'name':'Restaurant'})
-
-def register_ngo(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('name')
-            ngo = KhaanDaanUsers()
-            ngo.user = User.objects.get(email=form.cleaned_data.get('email'))
-            ngo.name = form.cleaned_data.get('name')
-            #ngo.uid = form.cleaned_data.get('uid')
-            ngo.mobile_no = form.cleaned_data.get('mobile')
-            ngo.email_id = form.cleaned_data.get('email')
-            ngo.address = form.cleaned_data.get('address')
-            ngo.user_type = form.cleaned_data.get('user_type')
-            ngo.save()
-            messages.success(request,f'Account created successfully for {username}!')
-            return redirect('login')
-
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form':form, 'name':'NGO'})
