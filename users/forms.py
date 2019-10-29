5afb8db520616950eb262890e253bb18d6b2f5f7
Zenil Haria <zenil.haria@somaiya.edu>
2019-10-30 02:38:37 +0530

    Removed Redundancies and Refined the code.

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     name = forms.CharField(required=True)
-    #uid = forms.CharField(required=True)
     mobile = forms.CharField(required=True)
     address = forms.CharField(required=True)
     user_type = forms.ChoiceField(choices=(('R','Restaurant'),('N','NGO')), required=True)
@@ -14,12 +13,3 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['name', 'user_type', 'mobile', 'email', 'address','username', 'password1','password2']
-
-'''
-class NGOForm(forms.ModelForm):
-    class Meta:
-        model = NGO
-        password = forms.CharField(widget=forms.PasswordInput)
-        confirm_password = forms.CharField(widget=forms.PasswordInput)
-        fields = ['name', 'nid', 'mobile_no', 'email_id', 'address', 'password']
-'''
