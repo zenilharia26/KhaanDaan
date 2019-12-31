7257e230ba56b2d60cb86046d46e9757e6cb39ba
Zenil Haria <zenil.haria@somaiya.edu>
2019-12-31 12:23:50 +0530

    Minor changes for deployment.

--- KhaanDaan/settings.py
+++ KhaanDaan/settings.py
@@ -129,7 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_FILES_DIR = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
