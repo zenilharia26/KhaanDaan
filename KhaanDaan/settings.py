8b2c22e459c3f142418bff260faf1d00d6a0e9f8
Zenil Haria <zenilharia06@gmail.com>
2020-04-19 02:39:49 +0530

    Settings.py changed.

--- KhaanDaan/settings.py
+++ KhaanDaan/settings.py
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'KhaanDaan.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
-DATABASES = {
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'KhaanDaan',
         'HOST': '127.0.0.1:27017',
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -137,7 +137,7 @@ STATIC_FILES_DIR = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
