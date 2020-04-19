40a32d85f1ab01d0dfd3cfefb342ccc2f75dd696
Zenil Haria <zenilharia06@gmail.com>
2020-04-19 14:29:02 +0530

    Static files removed.

--- KhaanDaan/settings.py
+++ KhaanDaan/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b9vixm^=k8ut^rkv!d7@5k7poazda&uhx2h4zr7j&al^8(=_5y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['khaan-daan.herokuapp.com']
 
