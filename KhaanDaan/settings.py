2e3a9657ea24f847b5fea3aea7bd189aa3e7bdf9
Zenil Haria <zenilharia06@gmail.com>
2020-04-19 12:39:51 +0530

    Minor changes.

--- KhaanDaan/settings.py
+++ KhaanDaan/settings.py
@@ -112,6 +112,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
